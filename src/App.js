836d59105408aada608dce4e7d5d57cf1faddc28
kirik <alkirichenko81@gmail.com>
2021-11-03 16:52:00 +0200

    003 Поэтапное создание темы

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import './App.css';
 import MyComponent from "./MyComponent";
 import {createTheme, styled, ThemeProvider} from "@mui/material";
 
-const myTheme = createTheme({
+const paletteTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#0000FF',
+        },
+        secondary: {
+            main: '#FF0000',
+        },
+        neutral: {
+            main: '#64748B',
+        },
+    }
+})
+
+const myTheme = createTheme(paletteTheme, {
     components: {
         MyComponent: {
             styleOverrides: {
@@ -10,14 +24,24 @@ const myTheme = createTheme({
                     backgroundColor: 'yellow',
                     padding: '10px 20px',
                     borderRadius: '3px',
+                    margin: '10px',
                     '&:hover': {
                         boxShadow: '0 0 3px 0 #000000',
                     },
-                    '@media(min-width: 768px)': {
-                        backgroundColor: 'yellowgreen',
-                    },
                 },
-            }
+                primary: {
+                    backgroundColor: paletteTheme.palette.primary.main,
+                    color: 'white',
+                },
+                secondary: {
+                    backgroundColor: paletteTheme.palette.secondary.main,
+                    color: 'white',
+                },
+                neutral: {
+                    backgroundColor: paletteTheme.palette.neutral.main,
+                    color: 'white',
+                },
+            },
         }
     }
 })
@@ -33,7 +57,10 @@ function App() {
     return (
         <ThemeProvider theme={myTheme}>
             <Wrapper>
-                <MyComponent>This is my component!</MyComponent>
+                <MyComponent>root</MyComponent>
+                <MyComponent variant={'primary'}>primary</MyComponent>
+                <MyComponent variant={'secondary'}>secondary</MyComponent>
+                <MyComponent variant={'neutral'}>secondary</MyComponent>
             </Wrapper>
         </ThemeProvider>
     );
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,15 +3,19 @@ import {styled} from "@mui/material";
 
 
 const MyComponentStyled = styled('div', {
+    shouldForwardProp: (prop) => prop !== 'variant',
     name: 'MyComponent',
     slot: 'Root',
     overridesResolver: (props, styles) => [
         styles.root,
+        props.variant === 'primary' && styles.primary,
+        props.variant === 'secondary' && styles.secondary,
+        props.variant === 'neutral' && styles.neutral,
     ]
 })(() => ({}))
 
-const MyComponent = ({children}) => {
-    return <MyComponentStyled>{children}</MyComponentStyled>
+const MyComponent = ({children, ...rest}) => {
+    return <MyComponentStyled {...rest}>{children}</MyComponentStyled>
 }
 
 export default MyComponent
